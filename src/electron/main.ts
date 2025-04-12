62f3594b07e4af04d24f5d2a8c36e24667c64315
Muhammad Usman <musman0741@gmail.com>
2025-04-12 12:14:15 +0500

    [task/reading-system-resources]: feat: add resource management functions and integrate polling for system resources

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path';
 import { isDev } from './util.js';
+import { pollResources } from './resourceManager.js';
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
@@ -17,4 +18,6 @@ app.on('ready', () => {
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
   }
+
+  pollResources();
 });
