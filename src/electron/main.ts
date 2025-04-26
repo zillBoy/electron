43c291e784a315c4b8166c1cdbedcfa81628dd7b
Muhammad Usman <musman0741@gmail.com>
2025-04-26 13:36:41 +0500

    [tray-icon]: feat: implement tray functionality and application menu; enhance IPC for frame actions and view changes

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,10 @@
-import { app, BrowserWindow, Tray } from 'electron'
-import path from 'path';
-import { ipcMainHandle, isDev } from './util.js';
+import { app, BrowserWindow } from 'electron'
+import { ipcMainHandle, ipcMainOn, isDev } from './util.js';
 import { getStaticData, pollResources } from './resourceManager.js';
-import { getAssetPath, getPreloadPath, getUIPath } from './pathResolver.js';
+import { getPreloadPath, getUIPath } from './pathResolver.js';
+import { createTray } from './tray.js';
+
+// Menu.setApplicationMenu(null);
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
@@ -25,5 +27,43 @@ app.on('ready', () => {
     return getStaticData();
   })
 
-  new Tray(path.join(getAssetPath(), 'icon@3x.png')).setToolTip('My Electron App')
+  ipcMainOn('sendFrameAction', (payload) => {
+    switch (payload) {
+      case 'CLOSE':
+        mainWindow.close();
+        break;
+      case 'MAXIMIZE':
+        mainWindow.maximize();
+        break;
+      case 'MINIMIZE':
+        mainWindow.minimize();
+        break;
+    }
+  });
+
+  createTray(mainWindow);
+  handleCloseEvents(mainWindow);
 });
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on('close', (e) => {
+    if (willClose) {
+      return;
+    }
+    e.preventDefault();
+    mainWindow.hide();
+    if (app.dock) {
+      app.dock.hide();
+    }
+  });
+
+  app.on('before-quit', () => {
+    willClose = true;
+  });
+
+  mainWindow.on('show', () => {
+    willClose = false;
+  });
+}
--- types.d.ts
+++ types.d.ts
@@ -1,35 +1,37 @@
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
-
 type Statistics = {
   cpuUsage: number;
   ramUsage: number;
-  storageData: number;
+  storageUsage: number;
 };
 
 type StaticData = {
   totalStorage: number;
   cpuModel: string;
   totalMemoryGB: number;
-}
+};
+
+type View = 'CPU' | 'RAM' | 'STORAGE';
+
+type FrameWindowAction = 'CLOSE' | 'MAXIMIZE' | 'MINIMIZE';
 
 type EventPayloadMapping = {
   statistics: Statistics;
   getStaticData: StaticData;
-}
+  changeView: View;
+  sendFrameAction: FrameWindowAction;
+};
 
-type UnSubscribeFunction = () => void;
+type UnsubscribeFunction = () => void;
 
 interface Window {
   electron: {
-    subscribeStatistics: (callback: (statistics: Statistics) => void) => UnSubscribeFunction;
+    subscribeStatistics: (
+      callback: (statistics: Statistics) => void
+    ) => UnsubscribeFunction;
     getStaticData: () => Promise<StaticData>;
-  }
+    subscribeChangeView: (
+      callback: (view: View) => void
+    ) => UnsubscribeFunction;
+    sendFrameAction: (payload: FrameWindowAction) => void;
+  };
 }
