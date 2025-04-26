d99a3c3188d6e106c574d0cdfb1eb255c909ac15
Muhammad Usman <musman0741@gmail.com>
2025-04-26 13:41:23 +0500

    [tray-icon]: refactor: streamline IPC functions and improve code readability in preload and resourceManager modules

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,45 +1,44 @@
-import { BrowserWindow } from 'electron';
-import osUtils from 'os-utils'
+import osUtils from 'os-utils';
 import os from 'os';
-import fs from 'fs'
+import fs from 'fs';
+import { BrowserWindow } from 'electron';
 import { ipcWebContentsSend } from './util.js';
 
 const POLLING_INTERVAL = 500;
 
 export function pollResources(mainWindow: BrowserWindow) {
   setInterval(async () => {
-    const cpuUsage = await getCpuUsage()
-    const ramUsage = getRamUsage()
+    const cpuUsage = await getCpuUsage();
+    const ramUsage = getRamUsage();
     const storageData = getStorageData();
-
     ipcWebContentsSend('statistics', mainWindow.webContents, {
       cpuUsage,
       ramUsage,
-      storageData: storageData.usage,
-    })
-  }, POLLING_INTERVAL)
+      storageUsage: storageData.usage,
+    });
+  }, POLLING_INTERVAL);
 }
 
 export function getStaticData() {
   const totalStorage = getStorageData().total;
   const cpuModel = os.cpus()[0].model;
   const totalMemoryGB = Math.floor(osUtils.totalmem() / 1024);
-  
+
   return {
     totalStorage,
     cpuModel,
-    totalMemoryGB
-  }
+    totalMemoryGB,
+  };
 }
 
 function getCpuUsage(): Promise<number> {
   return new Promise((resolve) => {
     osUtils.cpuUsage(resolve);
-  })
+  });
 }
 
 function getRamUsage() {
-  return 1 - osUtils.freememPercentage()
+  return 1 - osUtils.freememPercentage();
 }
 
 function getStorageData() {
@@ -50,6 +49,6 @@ function getStorageData() {
 
   return {
     total: Math.floor(total / 1_000_000_000),
-    usage: 1 - free / total
-  }
+    usage: 1 - free / total,
+  };
 }
